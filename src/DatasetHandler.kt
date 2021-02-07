9b83d23f44860bdac4b22a7ad469cc52fc52fb5b
Endryu <ipodendriu@gmail.com>
2021-02-07 22:46:30 +0000

    gui start2

--- src/DatasetHandler.kt
+++ src/DatasetHandler.kt
@@ -3,6 +3,7 @@ class DatasetHandler {
     val path = System.getProperties()
     var artistDataset = "$path/data_by_artist.csv"
 
+
     fun readFile()
             = File(artistDataset).forEachLine {print(it) }
 
