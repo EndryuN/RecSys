254eb9b30565aa54cef974e626a2493d0840c8e6
Endryu <ipodendriu@gmail.com>
2021-02-10 00:17:42 +0000

    song search complete

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -1,17 +1,23 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class MainGUI extends JFrame {
     private JPanel mainPanel;
     private JPanel northPanel;
     private JPanel centerPanel;
     private JButton searchBtn;
-    private JTextField textField1;
-    private JComboBox comboBox1;
-    private JButton loadBtn;
+    private JTextField searchTxt;
+    private JComboBox artistSelectCombo;
+    private JComboBox songSelectCombo;
+    private JButton saveBtn;
     private JTable songTbl;
     private JTable resultTbl;
+    private JButton getSongsButton;
+    private String searchInput;
+    private DefaultTableModel songModel;
 
     public MainGUI() {
         System.out.println("--------------------------\nSetting up GUI:");
@@ -22,30 +28,79 @@ public class MainGUI extends JFrame {
         setTitle("Recommendation system");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+
+        //Setting up tables
+        createTable();
+
+        //Loading dataset
+
         searchBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) { searchButtonPressed();
 
             }
         });
-        loadBtn.addActionListener(new ActionListener() {
+        saveBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) { loadButtonPressed();
+
+            }
+        });
+        getSongsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) { getSongsButtonPressed();
 
             }
         });
     }
     private void searchButtonPressed(){
         System.out.println("Search button pressed");
-
+        artistSelectCombo.removeAllItems();
+        populateComboBox();
+    }
+    private void getSongsButtonPressed(){
+        Main.datasetHandler.loadSongs();
+        Main.songHandler.saveSongs();
+        Main.songHandler.loadSongs();
     }
 
     private void loadButtonPressed(){
         System.out.println("Load button pressed");
-        Main.datasetHandler.loadArtists();
+        songSelectCombo.removeAllItems();
+        populateComboBox2();
 
     }
 
+
+    private void populateComboBox() {
+        System.out.println("SearchArtist.populateComboBox");
+        searchInput = searchTxt.getText();
+        for(int i=0;i<Main.artistHandler.getArtists().size();i++) {
+            if (Main.artistHandler.getArtists().get(i).getArtistName().toLowerCase().contains(searchInput.toLowerCase())) {
+                artistSelectCombo.addItem(Main.artistHandler.getArtists().get(i).getArtistName());
+            }
+        }
+    }
+
+    private void populateComboBox2() {
+        System.out.println("SearchSong.populateComboBox");
+        String selectedArtist = artistSelectCombo.getSelectedItem().toString();
+        for(int i=0;i<Main.songHandler.getSongs().size();i++) {
+            if (Main.songHandler.getSongs().get(i).getArtistName().toLowerCase().contains(selectedArtist.toLowerCase())) {
+                songSelectCombo.addItem(Main.songHandler.getSongs().get(i).getSongTitle());
+                System.out.println(Main.songHandler.getSongs().get(i).getSongTitle());
+                System.out.println(Main.songHandler.getSongs().get(i));
+            }
+        }
+    }
+
+    private void createTable() {
+        System.out.println("Creating Table");
+        String[] columnNames = {"Artist Name", "Title"};
+        songTbl.setModel(new DefaultTableModel(null, columnNames));
+        songModel = (DefaultTableModel) songTbl.getModel();
+
+    }
 
 
 }
